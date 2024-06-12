624893d31f9bc14f901ee170610f1abb294ab4ba
swisi <swisi71@gmail.com>
2024-06-12 23:51:46 +0200

    .

--- config.py
+++ config.py
@@ -3,5 +3,6 @@ import os
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'yBK#cqT^hv4hVDu@Ns3eBLRRnK654y86AHY7'
 
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'sqlite:///database/site.db')
+    basedir = os.path.abspath(os.path.join(os.path.dirname(__file__), '.'))  # Ein Verzeichnis höher
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', f'sqlite:///{os.path.join(basedir, "app", "database", "site.db")}')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
--- init_db.py
+++ init_db.py
@@ -1,9 +1,21 @@
-from app import db
+import os
+from app import create_app, db
 from app.models import User, Participant
 
 def init_db():
-    db.create_all()
-    print("Database initialized!")
+    app = create_app()
+    with app.app_context():
+        # Überprüfen und Erstellen des Verzeichnisses für die Datenbank
+        db_dir = os.path.join(app.root_path, 'database')
+        if not os.path.exists(db_dir):
+            os.makedirs(db_dir)
+        
+        # Geben Sie den Pfad zur Datenbankdatei aus
+        print(f"Database URI: {app.config['SQLALCHEMY_DATABASE_URI']}")
+        
+        # Initialisieren der Datenbank
+        db.create_all()
+        print("Database initialized!")
 
 if __name__ == "__main__":
     init_db()
