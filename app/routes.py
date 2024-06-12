9313e3023ddab8ce576b8762168a3e7adcd9c38d
swisi <swisi71@gmail.com>
2024-06-12 22:00:33 +0200

    Eingabemaske und Teilnehmererfassung ok

--- app/routes.py
+++ app/routes.py
@@ -12,21 +12,8 @@ bp = Blueprint('main', __name__)
 @login_required
 def index():
     participants = Participant.query.all() if current_user.is_authenticated else []
+    participants.sort(key=lambda p: p.longest_time if p.longest_time is not None else 0, reverse=True)
     return render_template('index.html', title='Home', participants=participants)
-
-@bp.route('/login', methods=['GET', 'POST'])
-def login():
-    if current_user.is_authenticated:
-        return redirect(url_for('main.index'))
-    form = LoginForm()
-    if form.validate_on_submit():
-        user = User.query.filter_by(username=form.username.data).first()
-        if user is None or not user.check_password(form.password.data):
-            flash('Invalid username or password')
-            return redirect(url_for('main.login'))
-        login_user(user, remember=form.remember_me.data)
-        return redirect(url_for('main.index'))
-    return render_template('login.html', title='Sign In', form=form)
 
 @bp.route('/logout')
 def logout():
