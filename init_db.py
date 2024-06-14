ba77fd899e801bd392ed632375ccd43e61be87a5
Martin Schweizer <swisi71@gmail.com>
2024-06-14 12:56:28 +0200

    .

--- init_db.py
+++ init_db.py
@@ -1,21 +1,8 @@
-import os
-from app import create_app, db
-from app.models import User, Participant
+from app import db
 
 def init_db():
-    app = create_app()
-    with app.app_context():
-        # Überprüfen und Erstellen des Verzeichnisses für die Datenbank
-        db_dir = os.path.join(app.root_path, 'database')
-        if not os.path.exists(db_dir):
-            os.makedirs(db_dir)
-        
-        # Geben Sie den Pfad zur Datenbankdatei aus
-        print(f"Database URI: {app.config['SQLALCHEMY_DATABASE_URI']}")
-        
-        # Initialisieren der Datenbank
-        db.create_all()
-        print("Database initialized!")
+    # Stellen Sie sicher, dass der Pfad zur Datenbankdatei korrekt ist
+    db.create_all()
 
 if __name__ == "__main__":
     init_db()
