2e4271037e116ad2df582588b0d825ba6b6099df
Martin Schweizer <swisi71@gmail.com>
2024-06-14 09:45:55 +0200

    docker-compose.yml externe Database

--- config.py
+++ config.py
@@ -3,6 +3,6 @@ import os
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'yBK#cqT^hv4hVDu@Ns3eBLRRnK654y86AHY7'
 
-    basedir = os.path.abspath(os.path.join(os.path.dirname(__file__), '.'))  # Ein Verzeichnis höher
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', f'sqlite:///{os.path.join(basedir, "app", "database", "site.db")}')
+    basedir = os.path.abspath(os.path.dirname(__file__))
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'sqlite:///' + os.path.join(basedir, 'database', 'site.db'))
     SQLALCHEMY_TRACK_MODIFICATIONS = False
